3fbe65725b7bf05f52830f726cf199787851312b
Qusai Al Shidi <qusai.alshidi@gmail.com>
2019-05-01 14:03:02 -0400

    Following correct order.

--- stitch-together.sh
+++ stitch-together.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm phd-dissertation.pdf
-pdftk A=signature-title-page.pdf B=abstract-title-page.pdf P=phd-dissertation-body.pdf cat A B P2-end output phd-dissertation.pdf
+pdftk A=title-page.pdf B=copyright.pdf C=signature-title-page.pdf D=abstract-title-page.pdf P=phd-dissertation-body.pdf cat A B C D P2-end output phd-dissertation.pdf
