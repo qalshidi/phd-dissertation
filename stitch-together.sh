9b9b21c08d9cdb0ef5bbfa19012dfb8eec6faa05
Qusai Al Shidi <qusai.alshidi@gmail.com>
2019-05-02 11:11:23 -0400

    Abstract title page shouldn't exist

--- stitch-together.sh
+++ stitch-together.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm phd-dissertation.pdf
-pdftk A=title-page.pdf B=copyright.pdf C=signature-title-page.pdf D=abstract-title-page.pdf P=phd-dissertation-body.pdf cat A B C D P2-end output phd-dissertation.pdf
+pdftk A=title-page.pdf B=copyright.pdf C=signature-title-page.pdf P=phd-dissertation-body.pdf cat A B C P2-end output phd-dissertation.pdf
